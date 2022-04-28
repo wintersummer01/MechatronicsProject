4dede5bdc59b641c2932fe06844d3b7ff2532865
elijahchoi1030 <elijah1030@yonsei.ac.kr>
2022-04-28 21:17:47 +0900

    Week1 Finished

--- src/exampleTeam.py
+++ src/exampleTeam.py
@@ -77,3 +77,59 @@ class StupidAgent(CaptureAgent):
                 FoodPosition = food
 
         return FoodPosition
+
+
+class JUNK_DummyAgent(CaptureAgent):
+  """
+  A Dummy agent to serve as an example of the necessary agent structure.
+  You should look at baselineTeam.py for more details about how to
+  create an agent as this is the bare minimum.
+  """
+
+  def registerInitialState(self, gameState):
+    """
+    This method handles the initial setup of the
+    agent to populate useful fields (such as what team
+    we're on).
+
+    A distanceCalculator instance caches the maze distances
+    between each pair of positions, so your agents can use:
+    self.distancer.getDistance(p1, p2)
+
+    IMPORTANT: This method may run for at most 15 seconds.
+    """
+
+    '''
+    Make sure you do not delete the following line. If you would like to
+    use Manhattan distances instead of maze distances in order to save
+    on initialization time, please take a look at
+    CaptureAgent.registerInitialState in captureAgents.py.
+    '''
+    CaptureAgent.registerInitialState(self, gameState)
+
+    '''
+    Your initialization code goes here, if you need any.
+    '''
+
+
+  def chooseAction(self, gameState):
+    """
+    Picks among actions randomly.
+    """
+    actions = gameState.getLegalActions(self.index)
+
+    '''
+    You should change this in your own agent.
+    '''
+
+    myState = gameState.getAgentState(self.index)
+    myPos = myState.getPosition()
+
+    print('Me :' + str(myPos))
+
+    print(gameState.hasFood(1, 1))
+
+    print(CaptureAgent.getScore(self, gameState))
+    print(self.getScore(gameState))
+
+    return random.choice(actions)
